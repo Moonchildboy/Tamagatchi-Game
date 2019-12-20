4f5cfdea2a34d77d69529055f2002608d70d1557
admin <admin@admins-MacBook-Pro.local>
2019-12-19 20:10:11 -0600

    basics of game works

--- js/app.js
+++ js/app.js
@@ -1,50 +1,92 @@
 // CLASS
 class Tamamgatchi {
-	constructor(hunger, sleepiness, boredom, age, name){
+	constructor(name){
 	this.hunger = 1 // perhaps set an array for these
 	this.sleepiness = 1 //-10 scale
 	this.boredom = 1 //-10 scale
-	this.age = 1 //-10 scale
+	this.age = 8 //-10 scale
 	this.name = name
 	}
 	// consider a method inside of this constructor
 }
+// argument is the thing we pass to a function
 
-const pet = new Tamamgatchi("cat")
-console.log(pet);
+// parameter is the thing the function receive as input
+
+
 
 // OBJECT
 const game = {
 
-	feed: 1,
-	sleep: 1,
-	play: 1, 
-	age: 1,
 	light: false, //
-	// METHOD 0 --> timer
+	tamamgatchi: null,
+	// METHOD 0 -->
+	startGame() {
+
+		this.tamamgatchi = new Tamamgatchi("cat")
+
+		this.timer()
+	},
+	// METHOD 1 --> timer
 	timer() {
 	const intervalId = setInterval(() => {
-  		this.feed += 1
-  		this.sleep += 1
-  		this.play += 1
-  		this.age += 1
-  		const $hungerElement = $("#hunger").text(this.feed) 
-  		const sleepElement = $("#sleepiness").text(this.sleep)
-  		const playElement = $("#boredom").text(this.play)
-  		const age = $("#age").text(this.age)
+
+  		this.tamamgatchi.hunger += 1
+  		this.tamamgatchi.sleepiness += 1
+  		this.tamamgatchi.boredom += 1
+  		this.tamamgatchi.age += 1
+
+  		// console.log(this.tamamgatchi);
+  		
+  		const $hungerElement = $("#hunger").text(this.tamamgatchi.hunger) 
+  		const $sleepElement = $("#sleepiness").text(this.tamamgatchi.sleepiness)
+  		const $playElement = $("#boredom").text(this.tamamgatchi.boredom)
+  		const $age = $("#age").text(this.tamamgatchi.age)
+		
+  		if (this.tamamgatchi.hunger === 10 || this.tamamgatchi.sleepiness === 10 || this.tamamgatchi.boredom === 10) {
+  			clearInterval(intervalId)
+  		}
     	}, 1000)
-	
-	// METHOD 1 --> Vitals should increment
+	},
 
-	// METHOD 2 --> Decrement based on buttons 
+	// METHOD 2 --> Vitals should decrement
+	feast() {
+		if (game.tamamgatchi.hunger >= 1 ) {
+			game.tamamgatchi.hunger -= 1
 
-	// METHOD 3 --> pet dies
+		}
+	},
+	sleep() {
+		if (game.tamamgatchi.sleep >= 1 ) {
+			game.tamamgatchi.sleep -= 1
+		}
+	},
+	play() {
+		if (game.tamamgatchi.play >= 1 ) {
+			game.tamamgatchi.play -= 1
+		}
+	}
+	// METHOD 3 --> Decrement based on buttons 
 
-	} // end of game object
+	// METHOD 4 --> pet dies
 
-}
+} // end of game object
 
-game.timer()
+
+
+game.startGame()
 
 // LISTENERS
 // click > 
+$("#feed").on("click", game.feast)
+$("#sleep").on("click", game.sleep)
+$("#play").on("click", game.play)
+
+
+
+
+
+
+
+
+
